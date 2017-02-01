5292977588b59dfbe7db42cb776e52d9c70c5f7e
Kirsten de Haas <kirstenlaradehaas@hotmail.com>
2017-02-01 18:48:52 +0100

    part 0 and 1

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
 const express = require ('express');
 const fs = require ('fs');
+const bodyParser = require ('body-parser')
 const app = express();
+
 
 //setting view folder
 app.set('views','./');
 
 // setting view engine
 app.set('view engine', 'pug');
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to ssupport URL-encoded bodies
+  extended: true
+})); 
 
-//do a request
+//do a get request 
 app.get('/', function (request, response){
 	console.log("about to render");
 	fs.readFile('./users.json', 'utf-8', function (error, data){
@@ -19,10 +25,23 @@ app.get('/', function (request, response){
 		}
 		//we need to parse users.json
 		const userList = JSON.parse(data)
-		response.render('index', {keyList:userList} );
+		response.render('index', {keyList:userList});
 	});
 });
 
-app.listen(3000, function() {
-	console.log('server has started');
+// do a request that renders a page and display a form
+app.get("/search", function(request, response){
+	console.log("ik werk ook ");
+	response.render('search');
 });
+
+//take post request from form displaying matches users on new page
+app.post("/signup", function(request, response){
+	console.log("ik werk")
+	console.log(request.body);
+	});
+
+app.listen(3000, function() {
+	console.log('server has started')s;
+});
+
